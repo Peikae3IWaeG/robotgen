66d3d255bfff4abce3971388e4eb120ca873deac
j <xxx@xxx.xx>
2023-07-27 02:32:28 +0200

    regex handling for commands

--- back/api/command.py
+++ back/api/command.py
@@ -1,8 +1,8 @@
 from generator.model.sections import TaskSectionGenerator
-from generator.model.keywords import RwCliRunCli
+from generator.model.keywords import RwCliRunCli, Catenate
 
 from os import getenv
-
+import re 
 from flask_restx import Resource, fields, api, Namespace, OrderedModel
 from typing import List
 
@@ -17,30 +17,48 @@ command = api.model(
     {
         "name": fields.String,
         "command": fields.String,
+        "regex": fields.String
     },
 )
 
 
 class CommandResource(object):
+    
     def __init__(self) -> None:
         self.commands: List(OrderedModel) = []
+
+    def drop(self) -> None:
+        self.commands = []
+
+    def get_command_by_name(self, name) -> OrderedModel:
+        return [x for x in self.commands if x['name']==name][0]
 
     def add(self, data) -> None:
         variable = data
         self.commands.append(variable)
+        
+    def dump_regex(self, name, regex):
+        if regex != "":
+            body = Catenate(assign_to_variable=True, variable="{}-regex".format(name), args=[regex])
+            return body 
+        return None
 
     def dump(self) -> TaskSectionGenerator:
         if len(self.commands) > 0:
-            body = [
-                RwCliRunCli(
-                    assign_to_variable=True,
-                    variable=x["name"],
-                    cmd=x["command"],
+            body = []
+            for x in self.commands:
+                body.append(
+                    RwCliRunCli(
+                        assign_to_variable=True,
+                        variable=x["name"],
+                        cmd=x["command"],
+                    )
                 )
-                for x in self.commands
-            ]
-
-            return body
+                regex = self.dump_regex(x['name'], x['regex'])
+                # print(regex)
+                if regex != None:
+                    body.append(regex)
+            return body 
         return []
 
 
@@ -77,7 +95,30 @@ class VarList(Resource):
                 api.payload["command"]
             )
         }
+        
+        regex = api.payload["regex"]    
         response = requests.request("GET", url, params=querystring)
-        result = response.json()["explanation"]
-        print(result)
+        gpt_explanation = response.json()["explanation"]
+        regex_numbered_explanation: List() = []
+        regex_named_explanation: List() = []
+        if regex != "":
+            try:        
+                for line in gpt_explanation.split('\n'):
+                    regexp_results = re.search(regex, line)
+                    if regexp_results:
+                        regex_numbered_explanation.append({line:regexp_results.groups()})
+                        regex_named_explanation.append(regexp_results.groupdict())
+            except Exception as e:
+                print(e)
+                
+        result = {
+            "gpt_explanation": gpt_explanation,
+            "regex_numbered_explanation": regex_numbered_explanation,
+            "regex_named_explanation": regex_named_explanation
+        }
+        
+        # print(regexp_results.groups())
+        
+        
+        # print(result)
         return result
